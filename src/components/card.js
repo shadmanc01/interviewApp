84eb8ad54e520e3a14452d472c245da7308ec27b
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2024-07-27 01:55:18 -0400

    feat: styling cards and adding color

--- src/components/card.js
+++ src/components/card.js
@@ -4,11 +4,11 @@ export default function Card(prop) {
     console.log(prop.item)
     return (
         <div class='card'>
-            <div class='container'>
-                <img src={prop.item.image_url} width='50%'/>
-               <h1>{prop.item.title}</h1>
-               <p>{prop.item.subtitle}</p>
-               <p>${prop.item.price}</p>
+            <div class='container' style={{padding:'2rem'}}>
+                <img src={prop.item.image_url} width='200rem'/>
+               <h1 style={{fontSize:'2.5rem'}}>{prop.item.title}</h1>
+               <p style={{fontSize:'1.5rem'}}>${prop.item.price}</p>
+               <button>Log Details</button>
             </div>
         </div>
     )
