af2cf1b3665d54ccd4b09844b9510d08b6644f0f
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2024-07-27 02:55:30 -0400

    finished cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,16 @@
 import "../style.css";
 
 export default function Card(prop) {
-  console.log(prop.item);
+  function handleClick() {
+    console.log(prop.item.title)
+  }
   return (
     <div class="card">
       <div class="product-container">
         <img class="prod-img" src={prop.item.image_url} />
         <h1 class="title">{prop.item.title}</h1>
         <p class="price">${prop.item.price}</p>
-        <button class="btn">Log Details</button>
+        <button onClick= {handleClick} class="btn">Log Details</button>
       </div>
     </div>
   );
