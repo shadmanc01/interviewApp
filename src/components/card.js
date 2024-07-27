4fc305a1090a383229f4fe0a0dea94d2d1abe8ad
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2024-07-27 02:28:57 -0400

    added styling to cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,15 @@
-import '../style.css'
+import "../style.css";
 
 export default function Card(prop) {
-    console.log(prop.item)
-    return (
-        <div class='card'>
-            <div class='container' style={{padding:'2rem'}}>
-                <img src={prop.item.image_url} width='200rem'/>
-               <h1 style={{fontSize:'2.5rem'}}>{prop.item.title}</h1>
-               <p style={{fontSize:'1.5rem'}}>${prop.item.price}</p>
-               <button>Log Details</button>
-            </div>
-        </div>
-    )
-} 
+  console.log(prop.item);
+  return (
+    <div class="card">
+      <div class="product-container">
+        <img class="prod-img" src={prop.item.image_url} />
+        <h1 class="title">{prop.item.title}</h1>
+        <p class="price">${prop.item.price}</p>
+        <button class="btn">Log Details</button>
+      </div>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,47 @@
- import { hydrate, prerender as ssr } from 'preact-iso';
- import { useState, useEffect } from 'preact/hooks';
-import Card from './components/card';
-import preactLogo from './assets/preact.svg';
-import './style.css';
-
-
+import { hydrate, prerender as ssr } from "preact-iso";
+import { useState, useEffect } from "preact/hooks";
+import Card from "./components/card";
+import preactLogo from "./assets/preact.svg";
+import "./style.css";
 
 export function App() {
+  const [result, setResult] = useState([]);
 
-	const [result, setResult] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch(
+        "https://www.ikea.com/global/assets/variera/shopping-experience-dev/se_test/data.json"
+      );
+      const jsonData = await data.json();
+      setResult(jsonData.items);
+    }
+    getData();
+  }, []);
 
-	useEffect(() => {
-		async function getData() {
-		  const data = await fetch(
-			'https://www.ikea.com/global/assets/variera/shopping-experience-dev/se_test/data.json'
-		  );
-		  const jsonData = await data.json();
-		  setResult(jsonData.items);
-		}
-		getData();
-	}, []);
-	
-	// console.log(result)
+  // console.log(result)
 
-	return (
-		
-		<div class='body'>
-			{result.map(product => (
-				<Card item = {product}/>
-			))}
-		</div>
-	);
+  return (
+    <div class="body">
+      {result.map((product) => (
+        <Card item={product} />
+      ))}
+    </div>
+  );
 }
 
 function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+  return (
+    <a href={props.href} target="_blank" class="resource">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </a>
+  );
 }
 
-if (typeof window !== 'undefined') {
-	hydrate(<App />, document.getElementById('app'));
+if (typeof window !== "undefined") {
+  hydrate(<App />, document.getElementById("app"));
 }
 
 export async function prerender(data) {
-	return await ssr(<App {...data} />);
+  return await ssr(<App {...data} />);
 }
